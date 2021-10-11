c90292a888d27c65b9eb21470f5fca780cb36d64
Fannoux <fanny.defranoux@gmail.com>
2021-10-11 19:02:19 +0200

    Change Genotype recuperation + add count verify function

--- bam_analysis.py
+++ bam_analysis.py
@@ -5,7 +5,7 @@ import os
 file_allele = 'all_nanopore_base_at_significant_snps_oncovmeth_05102021.txt'
 
 
-def count_table(df, t=0.95):
+def count_table(df, t=0.90):
     df['Gen'] = 'other'
     df.loc[df['Allele'] == df['ref'], 'Gen'] = 'ref'
     df.loc[df['Allele'] == df['alt'], 'Gen'] = 'alt'
@@ -15,31 +15,31 @@ def count_table(df, t=0.95):
         if df[df['Gen'] == haplo].shape[0] == 0:
             table_counts[haplo] = 0
     sum = table_counts[['alt', 'other', 'ref']].sum(axis=1)
+    percent = pd.DataFrame()
     table_counts['sum'] = sum
-    table_counts['alt%'] = table_counts['alt'].astype(int) / sum
-    table_counts['other%'] = table_counts['other'].astype(int) / sum
-    table_counts['ref%'] = table_counts['ref'].astype(int) / sum
+    percent['0'] = table_counts['ref'].astype(int) / sum
+    percent['1'] = table_counts['alt'].astype(int) / sum
+    percent['2'] = table_counts['other'].astype(int) / sum
+    percent = percent[percent > 1 - t]
+    for col in percent.columns:
+        percent.loc[(percent[col] == percent.max(axis=1)), 'Allele1'] = col
+        percent.loc[(percent[col] == percent.min(axis=1))
+                    & (percent['Allele1'] != col), 'Allele2'] = col
+        percent.loc[(percent.max(axis=1) == percent.min(axis=1))
+                    & (percent[col] == percent.max(axis=1))
+                    & (percent['Allele2'].isna()), 'Allele2'] = col
+    alleles = percent[['Allele1', 'Allele2']].astype(int).astype(str).copy()
+    percent['Genotype'] = alleles.min(axis=1) + '/' + alleles.max(axis=1)
+    return percent['Genotype'].reset_index()
 
-    table_counts.loc[(table_counts['alt%'] > 0.5 * t)
-                     & (table_counts['ref%'] < 1 * t), 'Genotype'] = '0/1'
-    table_counts.loc[(table_counts['ref%'] > 0.5 * t)
-                     & (table_counts['alt%'] < 1 * t), 'Genotype'] = '0/1'
 
-    table_counts.loc[(table_counts['ref%'] > 0.5 * t)
-                     & (table_counts['other%'] < 1 * t), 'Genotype'] = '0/2'
-    table_counts.loc[(table_counts['other%'] > 0.5 * t)
-                     & (table_counts['ref%'] < 1 * t), 'Genotype'] = '0/2'
-
-    table_counts.loc[(table_counts['other%'] > 0.5 * t)
-                     & (table_counts['alt%'] < 1 * t), 'Genotype'] = '1/2'
-    table_counts.loc[(table_counts['alt%'] > 0.5 * t)
-                     & (table_counts['other%'] < 1 * t), 'Genotype'] = '1/2'
-
-    table_counts.loc[table_counts['ref%'] > 1.0 * t, 'Genotype'] = '0/0'
-    table_counts.loc[table_counts['alt%'] > 1.0 * t, 'Genotype'] = '1/1'
-    table_counts.loc[table_counts['other%'] > 1.0 * t, 'Genotype'] = '2/2'
-
-    return table_counts
+def verify_counts(df):
+    counts = df.groupby(['name', 'Genotype', 'Gen']).size().unstack()
+    sum = counts.sum(axis=1)
+    counts['alt'] = counts['alt'] / sum
+    counts['ref'] = counts['ref'] / sum
+    counts['other'] = counts['other'] / sum
+    print(counts.to_markdown())
 
 
 def main(file_allele):
@@ -49,12 +49,12 @@ def main(file_allele):
                        error_bad_lines=False, header=None)
     df.columns = ['name', 'SNP', 'read_name', 'Allele']
     df[['CHR', 'pos', 'ref', 'alt']] = df['SNP'].str.split(':', expand=True)
-
     #Getting nanopolish file
     nano_cols = ['CHR', 'strand', 'start', 'end', 'read_name',
                  'log_lik_ratio', 'log_lik_methylated', 'log_lik_unmethylated',
                  'num_calling_strands', 'num_motifs', 'sequence']
     file_ls = set(df['name'].to_list())
+    file = 'gcc00022_PROM1'
     for file in file_ls:
         try:
             nano_file = os.path.join('nanopolish_grep_reads', file + '.txt')
@@ -67,9 +67,10 @@ def main(file_allele):
             nano_all = nano_all[(nano_all['pos'] < nano_all['start']-5)
                                 & (nano_all['pos'] < nano_all['end']+5)]
 
-            count_df = count_table(nano_all).reset_index()
-            merge = pd.merge(nano_all, count_df[['SNP', 'name', 'Genotype']], on=[
-                             'SNP', 'name'], copy=False)
+            count_df = count_table(nano_all)
+            merge = pd.merge(nano_all, count_df[['SNP', 'name', 'Genotype']],
+                             on=['SNP', 'name'], copy=False)
+            verify_counts(merge)
             if nano_all.shape[0] != merge.shape[0]:
                 print(f'{file} SNP-ID is not unique !')
             merge.to_csv(os.path.join('nanopolish_grep_reads',
@@ -80,7 +81,6 @@ def main(file_allele):
 
 if __name__ == '__main__':
     main(file_allele)
-
 
 # # Graphs
 # blou = df_global[df_global != 0]
