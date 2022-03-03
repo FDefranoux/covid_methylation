7e28420119d04dbb3be06bb85d4d330dcd14e1f4
Fannoux <fanny.defranoux@gmail.com>
2022-03-03 17:39:38 +0000

    simplify

--- quality_analysis.py
+++ quality_analysis.py
@@ -14,6 +14,7 @@ def verification_bsub_output(file, rerun_command='', add_comment=''):
     else:
         print(f'\n Unknown error - {add_comment}')
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Verification bsub outputs')
     parser.add_argument('--file', type=str, help='output file')
